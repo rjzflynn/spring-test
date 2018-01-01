ecd0d830d259f3f2c0a3562697e6e344e0a8d1f4
Richard Flynn <rjzflynn@gmail.com>
2018-01-01 20:17:02 +0000

    routing/display flow

--- src/angular-ui/src/app/app-routing.module.ts
+++ src/angular-ui/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ResturantGridComponent } from './resturant-grid/resturant-grid.component';
 import { TableBasicExample } from './table-basic-example/table-basic-example.component';
 
 
 const routes: Routes = [
+  { path: 'resturants', component: ResturantGridComponent },
+  { path: '', redirectTo: '/resturants', pathMatch: 'full' },
   { path: 'resturant/:id', component: TableBasicExample }
+
 ];
  
 @NgModule({
